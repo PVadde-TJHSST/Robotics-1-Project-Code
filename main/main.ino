a487184c3db23980a58ba156eda1550801b4ac1c
2024pvadde <2024pvadde@tjhsst.edu>
2021-10-24 14:54:34 -0400

    git bash test

--- main/main.ino
+++ main/main.ino
@@ -10,11 +10,9 @@ vexMotor botRight;
 
 void setup() {
 
-
-
-I2CEncoder encoder;
-Wire.begin();
-encoder.init(MOTOR_393_SPEED_ROTATIONS, MOTOR_393_TIME_DELTA);
+    I2CEncoder encoder;
+    Wire.begin();
+    encoder.init(MOTOR_393_SPEED_ROTATIONS, MOTOR_393_TIME_DELTA);
   
 }
 
